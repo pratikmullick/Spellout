c7631b3b64a6d9f8761b6ad4b04567fd21e9181b
Pratik <Pratik@prtktpadlinux.local>
2024-03-29 04:23:27 +0530

    Works as expected in C.
    TODO
    - Check on Windows 64 (Clang, and MSVC)
    - Check on Windows XP, and even DOS (Why not?)
    - Need to test for memory leaks. Find out ways to free output.

--- spellout.c
+++ spellout.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <math.h>
 
-// COMPILES UNDER CLANG W64, DOESN'T RUN
+// CURRENT VERSION WITH MALLOC SHOULD HAVE MEMORY LEAKS?
+
+#define MAG_MAX 11
 
 const char * uniques[] = {
                         "zero","one","two","three","four","five","six",
@@ -16,10 +20,17 @@ const char * base[] = {
 
 const char * magnitude[] = {"hundred","thousand","million","billion","trillion"};
 
-char * base_wording(int number) {
-    // Spells out the base, from 0 to 99
-    char * output = "";
+// Function Prototypes
+char * base_wording(int number);
+char * huns_wording(int number);
+char * spellout(long long number, int i);
+
+char * base_wording(int number)
+{
     int digit, units, tens;
+    char * teen = (char *) malloc(10 * sizeof(char));
+    char * ten_w = (char *) malloc(10 * sizeof(char));
+    char * output = (char *) malloc(20 * sizeof(char));
 
     if (number <= 13)
         strcpy(output, uniques[number]);
@@ -29,49 +40,94 @@ char * base_wording(int number) {
         strcpy(output, uniques[15]);
     else if (number <= 19)  {
         digit = number % 10;
-        strcpy(output, strcat(uniques[digit], "teen"));
+        strcpy(teen, uniques[digit]);
+        strcpy(output, strcat(teen, "teen"));
     }
-
     if (number >= 20)   {
         tens = number / 10;
-        strcat(output, strcat(base[tens - 2], " "));
+        strcpy(ten_w, base[tens - 2]);
+        strcat(ten_w, " ");
+        strcat(output, ten_w);
         if (number % 10 > 0)    {
             units = number % 10;
             strcat(output, uniques[units]);
         }
     }
+    free(teen);
+    free(ten_w);
+    return output;
+
+}
+
+char * huns_wording(int number)
+{
+    char * hun_w = (char *) malloc(20 * sizeof(char));
+    char * output = (char *) malloc(50 * sizeof(char));
     
-    return output;
-}
-/*
-char * huns_wording(int number) {
-    // Spells out hundreds values
-    char * output = "";
-    char * base = "";
-    int huns;
     int rem;
+    int huns = number / 100;
 
-    huns = number / 100;
-    if (number % 100 == 0)
-        strcpy(output, strcat(uniques[huns], " hundred"));
-    else if (number > 100)  {
-        strcpy(output, strcat(uniques[huns], " hundred and "));
+    if (number % 100 == 0)  {
+        strcpy(hun_w, uniques[huns]);
+        strcat(hun_w, " hundred");
+        strcpy(output, hun_w);
+    } else if (number > 100)    {
+        strcpy(hun_w, uniques[huns]);
+        strcat(hun_w, " hundred and ");
+        strcpy(output, hun_w);
         rem = number % 100;
-        // printf("Debug: %s\n", base_wording(rem));
+        if (rem > 0)
+            strcat(output, base_wording(rem));
+    } else  {
+        strcpy(output, base_wording(number));
     }
-
+    free(hun_w);
     return output;
 }
-*/
+
+char * spellout(long long number, int i)
+{
+    int num, group;
+    long long rem;
+
+    char * mag_w = (char *) malloc(50 * sizeof(char));
+    char * grp_w = (char *) malloc(50 * sizeof(char));
+    char * output = (char *) malloc(200 * sizeof(char));
+
+    while (i >= 0)  {
+        if (number % (long long) (pow(10, i)) != number)   {
+            num = 0;
+            group = i / 3;
+            num += (int) ((number / pow(10, i)) * pow(10, i % 3));
+            if (i % 3 == 0) {
+                strcpy(grp_w, huns_wording(num));
+                strcat(grp_w, " ");
+                strcat(output, grp_w);
+                if (group > 0)  {
+                    strcpy(mag_w, magnitude[group]);
+                    strcat(output, mag_w);
+                    strcat(output, " ");
+                }
+                rem = (long long) (number - num * pow(10, i));
+                number = rem;
+            }
+        }
+        if (i >= 0)
+            --i;
+    }
+    free(mag_w);
+    free(grp_w);
+    return output;
+}
 
 int main(void)
 {
-    int entry;
+    long long entry;
 
     printf("Enter a number: ");
-    scanf_s("%d", &entry);
+    (void) scanf("%lld", &entry);
 
-    printf("%s\n", base_wording(entry));
+    printf("%s\n", spellout(entry, MAG_MAX));
 
     return 0;
 }
